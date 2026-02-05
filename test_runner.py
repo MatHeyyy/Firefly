4ebace62d325c1e41fc0ebcf11ffdce5622eb3a8
Matei Costinescu <mcostinescu213@gmail.com>
2026-02-05 03:14:56 +0000

    Removed unneeded files from project

--- test_runner.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Test the refactored interpreter with a custom script"""
-
-from firefly.interpreter import FireflyInterpreter
-
-interpreter = FireflyInterpreter()
-interpreter.run_file('test_full.ff')
